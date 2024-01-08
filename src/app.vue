63cc7861672fb49344a3280cd5dd0f4950908202
“hanle” <hanle@go-goal.com>
2024-01-08 11:44:44 +0800

    update:config

---
+++ src/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h2>hahhah</h2>
+    <NuxtPage />
+    <!-- <NuxtWelcome /> -->
+  </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>页面</h2>
+    </div>
+</template>
+
+<script setup lang='ts'>
+defineOptions({
+    name: ''
+})
+</script>
+
+<style lang='less' scoped>
+</style>
